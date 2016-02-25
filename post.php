3179bc06c01326c1d54b97cd3a4a98239c1c15bf
Nikola Gorgiev <ngorgiev@live.com>
2016-02-25 23:14:19 +0100

    comments relation to posts

--- post.php
+++ post.php
@@ -18,7 +18,7 @@
                     $get_post_id = $_GET['p_id']; 
                 }
 
-                $query = "SELECT * FROM posts WHERE post_id = $get_post_id ";
+                $query = "SELECT * FROM posts WHERE post_id = $get_post_id";
 
                 $select_all_posts_query = mysqli_query($connection,$query);
                 while($row = mysqli_fetch_assoc($select_all_posts_query))
@@ -58,15 +58,44 @@
                 
                 
                 <!-- Blog Comments -->
+                
+                <?php
+                    if(isset($_POST['create_comment']))
+                    {
+                        $get_post_id = $_GET['p_id'];
+                        $comment_author = $_POST['comment_author'];
+                        $comment_email = $_POST['comment_email'];
+                        $comment_content = $_POST['comment_content'];
+                        
+                        $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status ,comment_date)";
+                        $query .= "VALUES ($get_post_id, '{$comment_author}', '{$comment_email}', '{$comment_content}', 'unaproved' ,now())";
+                        
+                        $create_comment_query = mysqli_query($connection, $query);
+                        
+                        if(!$create_comment_query)
+                        {
+                            die('QUERY FAILED' . mysqli_error($connection));
+                        }
+                    }
+                ?>
 
                 <!-- Comments Form -->
                 <div class="well">
                     <h4>Leave a Comment:</h4>
-                    <form role="form">
+                    <form action="" method="post" role="form">
                         <div class="form-group">
-                            <textarea class="form-control" rows="3"></textarea>
+                            <label for="Author">Author</label>
+                            <input type="text" name="comment_author" class="form-control" name="comment_author">
                         </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <div class="form-group">
+                            <label for="Author">Email</label>
+                            <input type="email" name="comment_email" class="form-control" name="comment_email">
+                        </div>
+                        <div class="form-group">
+                            <label for="comment">Your Comment</label>
+                            <textarea name="comment_content" class="form-control" rows="3"></textarea>
+                        </div>
+                        <button type="submit" name="create_comment" class="btn btn-primary">Submit</button>
                     </form>
                 </div>
 
