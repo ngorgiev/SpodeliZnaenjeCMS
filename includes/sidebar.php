9f225e895dd586b3b8ec842aa7f349e8a6dc1162
Nikola Gorgiev <ngorgiev@live.com>
2016-02-25 13:44:25 +0100

    SideBar CleanUp

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -26,7 +26,7 @@
                     ?>
                     <h4>Blog Categories</h4>
                     <div class="row">
-                        <div class="col-lg-6">
+                        <div class="col-lg-12">
                             <ul class="list-unstyled">
                                 <?php
                                 while($row = mysqli_fetch_assoc( $select_categories_sidebar))
@@ -37,20 +37,7 @@
                                 ?> 
                             </ul>
                         </div>
-                        <!-- /.col-lg-6 -->
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- /.col-lg-6 -->
+                       
                     </div>
                     <!-- /.row -->
                 </div>
