d629ea2282e59ff6895d601da320a1d651b10893
Nikola Gorgiev <ngorgiev@live.com>
2016-02-25 16:14:09 +0100

    update categories

--- admin/categories.php
+++ admin/categories.php
@@ -54,34 +54,15 @@
                             <input class="btn btn-primary" type="submit" name="submit" value="Add Category">   
                         </div>  
                         </form>
-                            
-                        <form action="" method="post">
-                        <div class="form-group">
-                            <label for="cat-title"> Edit Category</label>
-                            <?php
-                           
-                            if(isset($_GET['edit']))
-                            {
-                            $cat_id = $_GET['edit'];
-                                
-                            $query = "SELECT * FROM categories WHERE cat_id= $cat_id ";
-                            $select_categories_to_edit = mysqli_query($connection,$query); 
-
-                            while($row = mysqli_fetch_assoc($select_categories_to_edit))
-                            {
-                                $cat_id = $row['cat_id'];
-                                $cat_title = $row['cat_title'];
-                                ?>
-                            
-                            <input value="<?php if(isset($cat_title)){echo $cat_title; } ?>" type="text" class="form-control" name="cat-title">
-                            
-                            <?php }} ?>
-                       
-                        </div>
-                        <div class="form-group">
-                            <input class="btn btn-primary" type="submit" name="submit" value="Update Category">   
-                        </div>  
-                        </form>
+                        
+                        <?php 
+                        if(isset($_GET['edit']))
+                        {
+                            $cat_id = $_GET['edit'];   
+                            include "includes/update_categories.php";
+                        }
+                        ?>
+                        
                         </div>
                         <div class="col-xs-6">
                             
