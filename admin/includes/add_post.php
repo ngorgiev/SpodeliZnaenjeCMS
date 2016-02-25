e6c0cbcd6e1f2a312194c260fd978748476b9943
Nikola Gorgiev <ngorgiev@live.com>
2016-02-25 21:17:05 +0100

    categories in add page

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -3,7 +3,7 @@
     {
         $post_title = $_POST['title'];
         $post_author = $_POST['author'];
-        $post_category_id = $_POST['post_category_id'];
+        $post_category_id = $_POST['post_category'];
         $post_status = $_POST['post_status'];
         
         $post_image = $_FILES['image']['name'];
@@ -31,9 +31,29 @@
         <input type="text" class="form-control" name="title">
     </div>
     
-    <div class="form-group">
-        <label for="post_category">Post Category Id</label>
-        <input type="text" class="form-control" name="post_category_id">
+     <div class="form-group">
+        <select name="post_category" id="">
+            <?php 
+            $query = "SELECT * FROM categories";
+            $select_categories = mysqli_query($connection,$query); 
+            
+            confirmQuery($select_categories);
+
+            while($row = mysqli_fetch_assoc($select_categories))
+            {
+                $cat_id = $row['cat_id'];
+                $cat_title = $row['cat_title'];
+                if($cat_id == $post_category_id)
+                {
+                  echo "<option value='$cat_id' selected>{$cat_title}</option>";   
+                }
+                else
+                {
+                   echo "<option value='$cat_id'>{$cat_title}</option>";  
+                }
+            }
+            ?>
+        </select>
     </div>
     
     <div class="form-group">
