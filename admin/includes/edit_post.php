2dd2b62a8f4282c15fe3ba4aa252a218739eb52f
Nikola Gorgiev <ngorgiev@live.com>
2016-02-27 00:54:22 +0100

    edit post

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -97,9 +97,25 @@
         <input value="<?php echo $post_author; ?>" type="text" class="form-control" name="post_author">
     </div>
     
-    <div class="form-group">
+    <!-- <div class="form-group">
         <label for="post_status">Post Status</label>
         <input value="<?php echo $post_status; ?>" type="text" class="form-control" name="post_status">
+    </div> -->
+
+    <div class="form-group">
+        <select name="post_status">
+            <option value='<?php echo $post_status; ?>'><?php echo $post_status; ?></option>
+            <?php 
+                if($post_status == 'published')
+                {
+                    echo "<option value='draft'>Draft</option>"; 
+                }
+                else
+                {
+                    echo "<option value='published'>Published</option>";
+                }
+            ?>
+        </select>
     </div>
     
     <div class="form-group">
