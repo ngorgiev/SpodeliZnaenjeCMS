68c54e7bb1e09b26279c7dd3cf8e77566f301c03
Nikola Gorgiev <ngorgiev@live.com>
2016-02-26 21:36:21 +0100

    login to admin restrictions

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -1,12 +1,22 @@
-<?php ob_start(); ?>
 <?php include "../includes/db.php" ?>
 <?php include "functions.php" ?>
-<!DOCTYPE html>
+<?php ob_start(); ?>
+<?php session_start(); ?>
 
+<?php 
+    if(isset($_SESSION['user_role']))
+    {
+        if($_SESSION['user_role'] !== 'admin')
+        {
+            header("Location: ../index.php");
+        }
+    }
+?>
+
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -30,7 +40,5 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-
 </head>
-
 <body>
--- admin/index.php
+++ admin/index.php
@@ -1,34 +1,25 @@
 <?php include"includes/admin_header.php" ?>
 
-    <div id="wrapper">
-        
-       
+<div id="wrapper">
+    <!-- Navigation -->
+    <?php include "includes/admin_navigation.php" ?>
 
-        <!-- Navigation -->
-        <?php include "includes/admin_navigation.php" ?>
-
-        <div id="page-wrapper">
-
-            <div class="container-fluid">
-
-                <!-- Page Heading -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        
-                        
-                        <h1 class="page-header">
-                            Welcome to admin
-                            <small>Author </small>
-                        </h1>
-                       
-                    </div>
+    <div id="page-wrapper">
+        <div class="container-fluid">
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        Welcome to admin
+                        <small> <?php echo $_SESSION['username'] ?> </small>
+                    </h1>
                 </div>
-                <!-- /.row -->
-
             </div>
-            <!-- /.container-fluid -->
+            <!-- /.row -->
 
         </div>
-        <!-- /#page-wrapper -->
+        <!-- /.container-fluid -->
+    </div>
+    <!-- /#page-wrapper -->
 
-   <?php include "includes/admin_footer.php" ?>
+    <?php include "includes/admin_footer.php" ?>
